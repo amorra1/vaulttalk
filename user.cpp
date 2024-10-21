f46ff3509226c95f03fdc90772de026cb4edc575
Kirch77 <rylan.kirchmair@gmail.com>
2024-10-21 19:47:42 -0400

    changed and implmented the actual logic for registering and loggin in users. just have to now implement hashing for passwords and also connecting to python server for later.

--- user.cpp
+++ user.cpp
@@ -1,25 +1,56 @@
 #include "user.h"
 #include <iostream>
+#include <unordered_map>
+#include <functional> // for hash functions like SHA-256
+#include <sstream>
 
 using namespace std;
 
-//constructor
-User::User(string id, string name, string pubKey)
-    : userId(id), username(name), publicKey(pubKey) {}
+// Initing a static database
+unordered_map<string, User> User::userDatabase;
 
-string User::getUserId() const { //const ensures read only
-    return userId;
+User::User(string id, string name, string pubKey, string hashedPassword)
+    : userId(id), username(name), publicKey(pubKey), hashedPassword(hashedPassword) {}
+
+
+string User::getUserId() const { return userId; }
+string User::getUsername() const { return username; }
+string User::getPublicKey() const { return publicKey; }
+
+// compares hashed password
+bool User::authenticate(string passwordAttempt) const {
+    return hashedPassword == hashPassword(passwordAttempt);
 }
 
-string User::getUsername() const {
-    return username;
+//need to add a hashing function such as sha-256
+
+// for registering
+bool User::registerUser(const string &username, const string &password, const string &publicKey) {
+    if (userDatabase.find(username) != userDatabase.end()) {
+        cout << "Username is nto available!" << endl;
+        return false; // User already exists
+    }
+
+    string hashedPassword = hashPassword(password);
+    User newUser(username, username, publicKey, hashedPassword);
+    userDatabase[username] = newUser;
+    cout << "User " << username << " registered successfully!" << endl;
+    return true;
 }
 
-string User::getPublicKey() const {
-    return publicKey;
-}
+// for logging in an existing user
+User* User::loginUser(const string &username, const string &password) {
+    auto it = userDatabase.find(username);
+    if (it == userDatabase.end()) {
+        cout << "User is not found!" << endl;
+        return nullptr;
+    }
 
-//to test general output for now
-void User::displayUser() const {
-    cout << "UserID: " << userId << ", Username: " << username << endl;
+    if (it->second.authenticate(password)) {
+        cout << "Login successful!" << endl;
+        return &it->second;
+    } else {
+        cout << "Incorrect password!" << endl;
+        return nullptr;
+    }
 }
