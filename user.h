151476f123d59aee24a23f7b0dfba85acabd00b6
Kirch77 <rylan.kirchmair@gmail.com>
2024-10-21 15:08:13 -0400

    updated user.h file for data base and password

--- user.h
+++ user.h
@@ -2,24 +2,35 @@
 #define USER_H
 
 #include <string>
+#include <iostream>
+#include <unordered_map>
+
 
 using namespace std;
 
 class User {
-private:
-    string userId; //can be used for port identification
-    string username;
-    string publicKey;
-    // maybe add password or other user info as we go
-
 public:
-    User(string id, string name, string pubKey);
+    User(std::string id, std::string name, std::string pubKey, std::string hashedPassword);
 
-    string getUserId() const;
-    string getUsername() const;
-    string getPublicKey() const;
+    std::string getUserId() const;
+    std::string getUsername() const;
+    std::string getPublicKey() const;
 
-    void displayUser() const;
+    bool authenticate(std::string passwordAttempt) const;
+
+    static std::string hashPassword(const std::string &password);
+
+    // stuff for user database (sign up and login)
+    static bool registerUser(const std::string &username, const std::string &password, const std::string &publicKey);
+    static User* loginUser(const std::string &username, const std::string &password);
+
+private:
+    std::string userId;
+    std::string username;
+    std::string publicKey;
+    std::string hashedPassword;
+
+    static std::unordered_map<std::string, User> userDatabase; // Store registered users
 };
 
 #endif //USER_H
