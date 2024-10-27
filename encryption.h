3f636e0f55ee535d5471555429b1bfe89dd5a7b1
Whimble <brwyand@gmail.com>
2024-10-27 19:19:33 -0400

    Working on adding GMP functionality

--- encryption.h
+++ encryption.h
@@ -2,18 +2,19 @@
 #define ENCRYPTION_H
 
 #include <string>
+#include <gmpxx.h>
 
 typedef struct keys {
 
-    long long publicKey[2];
-    long long privateKey[2];
+    mpz_class publicKey[2];
+    mpz_class privateKey[2];
 
 }RSA_keys;
 
 namespace encryption {
 
 RSA_keys GenerateKeys();
-unsigned long long RSA_Encrypt(std::string inputMsg, RSA_keys keys);
+mpz_class RSA_Encrypt(std::string inputMsg, RSA_keys keys);
 std::string RSA_Decrypt(unsigned long long inputValue, RSA_keys keys);
 
 }
