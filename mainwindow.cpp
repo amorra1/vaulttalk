a1e8eafe36012ff85eb5b6626faa0a208d2bfab8
hubertstarosta <128559378+hubertstarosta@users.noreply.github.com>
2024-12-01 14:41:43 -0500

    UI updates

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -53,7 +53,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->regenDurationDropdown->addItems({"Never","Per session", "Daily", "Monthly", "Weekly"});
     ui->caesarShiftDropdown->addItems({"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                                   "11", "12", "13", "14", "15", "16", "17", "18", "19",
-                                  "20", "21", "22", "23", "24", "25", "26"});
+                                  "20", "21", "22", "23", "24", "25"});
     ui->caesarShiftDropdown->setCurrentText("1");
 
     // Connect buttons to switch between pages
